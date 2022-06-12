0f4eb7006b0277d8c75cff302b76e055dc85c749
Josh Holmes <josh@glowingmanagement.com>
2022-06-12 16:54:30 +0100

    create employee class with tests

---
+++ src/lib/Employee.test.js
@@ -0,0 +1,37 @@
+const Employee = require("./Employee");
+
+const employeeTest = new Employee({
+  name: "Bob",
+  id: "1",
+  email: "bob@email.com",
+});
+
+describe("Employee", () => {
+  it("should return 'Bob' when getName is called", () => {
+    const expected = "Bob";
+    const actual = employeeTest.getName();
+
+    expect(actual).toEqual(expected);
+  });
+
+  it("should return '1' when getId is called", () => {
+    const expected = "1";
+    const actual = employeeTest.getId();
+
+    expect(actual).toEqual(expected);
+  });
+
+  it("should return 'bob@email.com' when getEmail is called", () => {
+    const expected = "bob@email.com";
+    const actual = employeeTest.getEmail();
+
+    expect(actual).toEqual(expected);
+  });
+
+  it("should return 'Employee' when getRole is called", () => {
+    const expected = "Employee";
+    const actual = employeeTest.getRole();
+
+    expect(actual).toEqual(expected);
+  });
+});
