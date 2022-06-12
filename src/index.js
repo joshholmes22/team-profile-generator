62b761a9d4e948cdfc4d6f60591dc08a9a4831f5
Josh Holmes <josh@glowingmanagement.com>
2022-06-12 12:37:21 +0100

    employee class outline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log("Hello");
+
+class Employee {
+  constructor({ name, id, email }) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+  }
+
+  getName() {}
+
+  getId() {}
+
+  getEmail() {}
+
+  getRole() {
+    return "Employee";
+  }
+}
