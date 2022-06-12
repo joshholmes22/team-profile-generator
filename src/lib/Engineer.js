4601829c8398e9fafcceb78bae25f10d4f406e90
Josh Holmes <josh@glowingmanagement.com>
2022-06-12 17:42:12 +0100

    Engineer code & all tests pass

--- src/lib/Engineer.js
+++ src/lib/Engineer.js
@@ -1,45 +1,20 @@
-const Engineer = require("./Engineer");
+const Employee = require("./Employee");
 
-const engineerTest = new Engineer({
-  name: "Kate",
-  id: "4",
-  email: "kate@email.com",
-  school: "https://github.com/kate",
-});
+class Engineer extends Employee {
+  constructor({ name, id, email, link }) {
+    super({ name, id, email });
 
-describe("Engineer", () => {
-  it("should return 'Kate' when getName is called", () => {
-    const expected = "Kate";
-    const actual = engineerTest.getName();
+    this.link = link;
+    this.role = "Engineer";
+  }
 
-    expect(actual).toEqual(expected);
-  });
+  getLink() {
+    return this.link;
+  }
 
-  it("should return '4' when getId is called", () => {
-    const expected = "4";
-    const actual = engineerTest.getId();
+  getRole() {
+    return this.role;
+  }
+}
 
-    expect(actual).toEqual(expected);
-  });
-
-  it("should return 'kate@email.com' when getEmail is called", () => {
-    const expected = "kate@email.com";
-    const actual = engineerTest.getEmail();
-
-    expect(actual).toEqual(expected);
-  });
-
-  it("should return 'Engineer' when getRole is called", () => {
-    const expected = "Engineer";
-    const actual = engineerTest.getRole();
-
-    expect(actual).toEqual(expected);
-  });
-
-  it("should return 'https://github.com/kate' when getLink is called", () => {
-    const expected = "https://github.com/kate";
-    const actual = engineerTest.getLink();
-
-    expect(actual).toEqual(expected);
-  });
-});
+module.exports = Engineer;
